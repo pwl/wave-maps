24bde39b8b635ba2ad4f6b0ddc89bd4d7637a190
Paweł Biernat <pawel.biernat+github@gmail.com>
2014-09-17 16:40:57 +0200

    Added two new discretizations

--- discretization.jl
+++ discretization.jl
@@ -15,12 +15,12 @@ function dur(r,u)
              h[2]=r[i+1]#-r[i]
              h[3]=r[i+2]#-r[i]
             ur[i]=Du[2]/h[2] + (Du[3]-Du[2])/(h[2]-h[3]) + Du[3]/h[3]
-        elseif i == 2
-            Du[1]=-u[i]
-            Du[3]=u[i+1]-u[i]
-             h[1]=-r[i]
-             h[3]=r[i+1]-r[i]
-            ur[i]=Du[1]/h[1] + (Du[3]-Du[1])/(h[1]-h[3]) + Du[3]/h[3]
+        # elseif i == 2
+        #     Du[1]=-u[i]
+        #     Du[3]=u[i+1]-u[i]
+        #      h[1]=-r[i]
+        #      h[3]=r[i+1]-r[i]
+        #     ur[i]=Du[1]/h[1] + (Du[3]-Du[1])/(h[1]-h[3]) + Du[3]/h[3]
         elseif i == npts
             Du[1]=u[i-1]-u[i]
             Du[2]=u[i-2]-u[i]
@@ -37,6 +37,33 @@ function dur(r,u)
     end
 
     return ur
+end
+
+function durr(r,u)
+    order = 2
+    npts=length(r)
+    urr=zero(u)
+    Du=zeros(eltype(u),1+2*order)
+    h =zeros(eltype(u),1+2*order)
+    for i = 1:npts
+        if i == 1
+            urr[i]=0
+        elseif i == npts
+            Du[1]=u[i-1]-u[i]
+            Du[2]=u[i-2]-u[i]
+             h[1]=r[i-1]-r[i]
+             h[2]=r[i-2]-r[i]
+            urr[i]=(2*Du[1])/(h[1]*(h[1] - h[2])) - (2*Du[2])/((h[1] - h[2])*h[2])
+        else
+            Du[1]=u[i-1]-u[i]
+            Du[3]=u[i+1]-u[i]
+             h[1]=r[i-1]-r[i]
+             h[3]=r[i+1]-r[i]
+            urr[i]=(2*Du[1])/(h[1]*(h[1] - h[3])) - (2*Du[3])/((h[1] - h[3])*h[3])
+        end
+    end
+
+    return urr
 end
 
 # we assume that u is a symmetric function, i.e. u(r)=u(-r)
@@ -117,6 +144,24 @@ function L(d,r,u;order=2, args...)
                 h[5] =r[i+2]-r[i]
                 Lu[i]=((d-1)*Du[5]*h[1]*h[2]*h[4])/((h[1]-h[5])*h[5]*(-h[2]+h[5])*(-h[4]+h[5])*r[i])-(Du[1]*((d-1)*h[2]*h[4]*h[5]+2(h[2]+h[4])*(h[1]-h[5])*r[i]))/(h[1]*(h[1]-h[2])*(h[1]-h[4])*(h[1]-h[5])r[i])+(Du[2]*((d-1)*h[1]*h[4]*h[5]+2(h[1]+h[4])*(h[2]-h[5])*r[i]))/((h[1]-h[2])*h[2]*(h[2]-h[4])*(h[2]-h[5])*r[i])+(Du[4]*((d-1)h[1]*h[2]*h[5]+2(h[1]+h[2])*(h[4]-h[5])r[i]))/((h[1]-h[4])h[4]*(-h[2]+h[4])*(h[4]-h[5])r[i])
             end
+        end
+    end
+    return Lu
+end
+
+# approximates urr+(d-1)(ur/r-u/r^2)
+function Lu2(d,r,u)
+    npts = length(r)
+    Lu=zero(u)
+    for i = 1:npts
+        if i == 1
+            Lu[i]=0
+        elseif i == 2
+            Lu[i]=(2^d*(-(u[i]/r[i])+((r[i]+r[1+i])^(1-d)*(-r[i]^(-1+d)*u[i]+r[1+i]^(-1+d)*u[1+i]))/(-r[i]+r[1+i])))/r[1+i]
+        elseif i == npts
+            Lu[i]=(2^d*(-(((r[-2+i]+r[-1+i])^(1-d)*(-r[-2+i]^(-1+d)*u[-2+i]+r[-1+i]^(-1+d)*u[-1+i]))/(-r[-2+i]+r[-1+i]))+((r[-1+i]+r[i])^(1-d)*(-r[-1+i]^(-1+d)*u[-1+i]+r[i]^(-1+d)*u[i]))/(-r[-1+i]+r[i])))/(-r[-2+i]+r[i])
+        else
+            Lu[i]=(2^d*(-(((r[-1+i]+r[i])^(1-d)*(-r[-1+i]^(-1+d)*u[-1+i]+r[i]^(-1+d)*u[i]))/(-r[-1+i]+r[i]))+((r[i]+r[1+i])^(1-d)*(-r[i]^(-1+d)*u[i]+r[1+i]^(-1+d)*u[1+i]))/(-r[i]+r[1+i])))/(-r[-1+i]+r[1+i])
         end
     end
     return Lu
--- equations.jl
+++ equations.jl
@@ -12,13 +12,43 @@ function rhsWM(r,u)
     return dudt
 end
 
-const WM=Equation(rhsWM,2)
+function rhsWMv2(r,u)
+    npts = length(r)
+    dudt = zero(u)
+    u_rr   = durr(r,u[:,1])
+    ubyr   = u[:,1]./r
+    ubyr[1]= dur(r[1:3],u[1:3,1])[1]
+    ubyr_r = (d-1)*dur(r,ubyr)
+    nonln  = (d-1)*(2u[:,1]-sin(2u[:,1]))./r.^2 / 2
+
+    dudt[:,1] = u[:,2]
+    dudt[:,2] = u_rr+ubyr_r+nonln
+
+    return dudt
+end
+
+function rhsWMv3(r,u)
+    npts = length(r)
+    Lu   = Lu2(2,r,u[:,1])
+    ubyr   = u[:,1]./r
+    ubyr[1]= dur(r[1:3],u[1:3,1])[1]
+    ubyr_r = (d-2)*dur(r,ubyr)
+    nonln  = (d-1)*(2u[:,1]-sin(2u[:,1]))./r.^2 / 2
+    dudt = zero(u)
+    dudt[:,1] = u[:,2]
+    dudt[:,2] = Lu+ubyr_r+nonln
+    dudt[1,:] = 0
+    dudt[end,:] = 0
+    return dudt
+end
+
+const WM=Equation(rhsWMv3,2)
 
 # default monitor function
 function WMmonitor(r,u,ur)
-    M=sqrt(1.+abs(ur).^2*(d-2))
+    M=sqrt(1.+abs(ur).^2)
 end
 
 function WMsundman(u,ur)
-    1/sqrt(ur[1]^2+1)
+    1/sqrt((ur[1]*sqrt(d-2)/2)^2+1)
 end
